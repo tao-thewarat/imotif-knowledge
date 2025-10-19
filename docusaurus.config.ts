8b2ec25a59e3f2312e48df6583cb8aed77dfed9e
thewarat <thewarat@imotif.io>
2025-10-19 21:22:44 +0700

    [FIX] remove edit page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,8 +43,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: undefined,
         },
         blog: {
           showReadingTime: true,
